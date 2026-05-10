33fc0af247e4891ed0c0f7c6e839b960fcbb2ec7
OrionAstesia <1278276395@qq.com>
2026-05-10 16:16:58 +0800

    Init: backend

---
+++ backend/topic-channel-platform/src/test/java/com/zzmg/topicchannelplatform/TopicChannelPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzmg.topicchannelplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicChannelPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
