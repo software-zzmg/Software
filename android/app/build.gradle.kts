d4d4506f5296729fc57e245fe5d3105fb06acf81
OrionAstesia <1278276395@qq.com>
2026-05-24 21:50:44 +0800

    feat: 新增日期测试用例

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.zzmg.topic_channel_platform"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
